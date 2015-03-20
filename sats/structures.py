eef7a3f6b767057b4ca661b630912ea9dab76879
tdaff <tdaff@localhost>
2015-03-20 15:11:25 +0000

    Create dumbell with any lattice direction

--- sats/structures.py
+++ sats/structures.py
@@ -5,15 +5,19 @@ Helpers to create different starting environments.
 
 """
 
+import re
+
 import quippy.structures
 
 from sats.elements import Element
 
 
-def bcc_interstitial_111(lattice_parameter, species, interstitial_species=None,
-                         supercell=(1, 1, 1), index=-1):
+def bcc_interstitial_dumbell(lattice_parameter, species, direction='111',
+                             interstitial_species=None, supercell=(1, 1, 1),
+                             index=-1):
     """
-    Create a BCC crystal structure with a <111> dumbbell interstitial atom.
+    Create a BCC crystal structure with a dumbbell interstitial atom along
+    the given lattice direction.
 
     Parameters
     ----------
@@ -21,6 +25,8 @@ def bcc_interstitial_111(lattice_parameter, species, interstitial_species=None,
         BCC lattice parameter in A.
     species : int or str
         Identity of species used to construct the BCC lattice.
+    direction: str
+        Lattice direction along which to create the dumbbell.
     interstitial_species : int or str, optional
         Identity of the interstitial species; if not specified, creates a
         self-interstitial.
@@ -34,7 +40,7 @@ def bcc_interstitial_111(lattice_parameter, species, interstitial_species=None,
     Returns
     -------
     quippy.Atoms
-        BCC lattice with a 111 dumbbell interstitial atom.
+        BCC lattice with a dumbbell interstitial atom.
     """
 
     species = Element(species)
@@ -44,7 +50,7 @@ def bcc_interstitial_111(lattice_parameter, species, interstitial_species=None,
         interstitial_species = Element(interstitial_species)
 
     bulk = quippy.structures.bcc(a=lattice_parameter, z=species.atomic_number)
-    # catch all false values for supercell
+    # catch all False values for supercell
     if supercell:
         bulk = quippy.structures.supercell(bulk, supercell[0], supercell[1],
                                            supercell[2])
@@ -52,11 +58,15 @@ def bcc_interstitial_111(lattice_parameter, species, interstitial_species=None,
     if index < 0:
         index = bulk.n + index
 
-    # bcc 111 dumbbell
-    magnitude = (1.0/6)*lattice_parameter
-    displacement = [magnitude, magnitude, magnitude]
+    # parses the digits from any string, ignoring brackets etc.
+    direction = [int(x) for x in re.findall('[+-]*[0-9]', direction)]
+    displacement = [1.0/x if x else 0.0 for x in direction]
+    # Move 1/3 of the <111> atom separation in each direction
+    magnitude = lattice_parameter/(12*sum(x**2 for x in displacement))**0.5
+    displacement = [x*magnitude for x in displacement]
     bulk.add_atoms(pos=bulk[index].position + displacement,
                    z=interstitial_species.z)
     bulk[index].position -= displacement
 
     return bulk
+
