69be1eadc7d6d99c81a95e11eb8f24b59b7151c2
tdd20 <tdd20@cam.ac.uk>
2015-05-27 14:16:12 +0100

    put usage info where it is needed (after options are initialised)

--- sats/ui/__init__.py
+++ sats/ui/__init__.py
@@ -3,19 +3,3 @@ ui
 
 Helpers for user interfacing with sats.
 """
-
-import sys
-
-from sats.ui.args import get
-from options import format_defaults
-
-
-description = "sats structure generation tool. Generate some structures"
-
-if get('ui', 'help'):
-    print("usage: {0} [section.option=value ...]".format(sys.argv[0]))
-    print("")
-    print(description)
-    print("")
-    print("\n".join(format_defaults()))
-#    raise SystemExit
