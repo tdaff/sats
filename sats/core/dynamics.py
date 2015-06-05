9a1ec31015f46d880c80972640fdf8c7e88c30d2
Tom Daff <7022897+tdaff@users.noreply.github.com>
2015-06-05 15:46:04 +0100

    capturing quippy output to the debug stream

--- sats/core/dynamics.py
+++ sats/core/dynamics.py
@@ -18,6 +18,10 @@ from sats.ui.log import info, debug
 
 # CAPTURE quippy output
 class Capturing(list):
+    def __init__(self, debug_on_exit=False, *args, **kwargs):
+        self.debug_on_exit = debug_on_exit
+        super(Capturing, self).__init__(*args, **kwargs)
+
     def __enter__(self):
         # capture normal stdout and put in a pipe
         self.stdout_fileno = sys.stdout.fileno()
@@ -27,7 +31,8 @@ class Capturing(list):
         os.close(self.pipe_out)
         return self
 
-    def grab(self):
+    def update(self):
+        """Read any output so far and update the object."""
         lines = os.read(self.pipe_in, 99999).splitlines()
         self.extend(lines)
         return lines
@@ -38,7 +43,9 @@ class Capturing(list):
         os.close(self.pipe_in)
         os.dup2(self.stdout_save, self.stdout_fileno)
         os.close(self.stdout_save)
-
+        if self.debug_on_exit:
+            for line in self:
+                debug(line)
 
 def relax_structure(system, potential, relax_positions=True, relax_cell=True):
     """
@@ -49,8 +56,9 @@ def relax_structure(system, potential, relax_positions=True, relax_cell=True):
     system : ase.Atoms
         A system of atoms to run the minimisation on. The structure is
         altered in-place.
-    potential : str
-        The potential arg_str to pass to the quippy Potential object.
+    potential : Potential or str
+        A quippy Potential object with the desired potential, or a
+        potential_str to initialise a new potential.
 
     Returns
     -------
@@ -60,24 +68,23 @@ def relax_structure(system, potential, relax_positions=True, relax_cell=True):
     info("Inside minimiser.")
 
     system = Atoms(system)
-    run_potential = Potential(potential)
-    system.set_calculator(run_potential)
+
+    if not isinstance(potential, Potential):
+        potential = Potential(potential)
+    system.set_calculator(potential)
 
     minimiser = Minim(system, relax_positions=relax_positions,
                       relax_cell=relax_cell)
 
-    with Capturing() as output:
+    with Capturing(debug_on_exit=True):
         minimiser.run()
-
-    for statement in output:
-        debug(statement)
 
     info("Minimiser done.")
 
     return system
 
 
-def molecular_dynamics(system, potential_str, temperature, total_steps=1100000,
+def molecular_dynamics(system, potential, temperature, total_steps=1100000,
                        timestep=1.0, connect_interval=200, write_interval=20000,
                        equilibration_steps=100000):
     """
@@ -85,25 +92,30 @@ def molecular_dynamics(system, potential_str, temperature, total_steps=1100000,
     configurations out as xyz and CASTEP files.
     """
 
+    info("Inside MD.")
     system = Atoms(system)
 
-    potential = Potential(potential_str)
+    # Can take Potential objects, or just use a string
+    if not isinstance(potential, Potential):
+        potential = Potential(potential)
     system.set_calculator(potential)
 
     dynamical_system = DynamicalSystem(system)
-    dynamical_system.rescale_velo(temperature)
-    print(dynamical_system.atoms.velo.T)
+    with Capturing(debug_on_exit=True):
+        dynamical_system.rescale_velo(temperature)
+    # Stop things moving vertically in the cell
     dynamical_system.atoms.velo[3, :] = 0
-    print(dynamical_system.atoms.velo.T)
 
     base_dir = os.getcwd()
     run_path = '{0}_{1:g}/'.format(system.info['name'], temperature)
+    info("Putting files in {0}.".format(run_path))
     os.mkdir(run_path)
     os.chdir(run_path)
 
     trajectory = 'traj_{0}_{1:g}.xyz'.format(system.info['name'], temperature)
     out = AtomsWriter(trajectory)
 
+    dynamical_system.atoms.set_cutoff(potential.cutoff() + 2.0)
     dynamical_system.atoms.calc_connect()
     potential.calc(dynamical_system.atoms, force=True, energy=True, virial=True)
 
@@ -111,23 +123,30 @@ def molecular_dynamics(system, potential_str, temperature, total_steps=1100000,
 
     # Basic NVE molecular dynamics
     for step_number in range(1, total_steps + 1):
-        dynamical_system.advance_verlet1(timestep,
-                                         virial=dynamical_system.atoms.virial)
+        dynamical_system.advance_verlet1(
+            timestep, virial=dynamical_system.atoms.virial)
         potential.calc(dynamical_system.atoms, force=True, energy=True,
                        virial=True)
-        dynamical_system.advance_verlet2(timestep,
-                                         f=dynamical_system.atoms.force,
-                                         virial=dynamical_system.atoms.virial)
+        dynamical_system.advance_verlet2(
+            timestep, f=dynamical_system.atoms.force,
+            virial=dynamical_system.atoms.virial)
 
         # Maintenance of the system
         if not step_number % connect_interval:
+            debug("Connect at step {0}".format(step_number))
             dynamical_system.atoms.calc_connect()
             if step_number < equilibration_steps:
-                dynamical_system.rescale_velo(temperature)
+                with Capturing(debug_on_exit=True):
+                    dynamical_system.rescale_velo(temperature)
 
         if not step_number % write_interval:
-            dynamical_system.print_status(epot=dynamical_system.atoms.energy)
-            dynamical_system.rescale_velo(temperature)
+            debug("Write at step {0}".format(step_number))
+            # Print goes to captured stdout
+            with Capturing(debug_on_exit=True):
+                dynamical_system.print_status(
+                    epot=dynamical_system.atoms.energy)
+                dynamical_system.rescale_velo(temperature)
+
             if step_number > equilibration_steps:
                 out.write(dynamical_system.atoms)
                 sp_path = '{0:03d}'.format(structure_count)
@@ -137,8 +156,11 @@ def molecular_dynamics(system, potential_str, temperature, total_steps=1100000,
                 os.chdir(sp_path)
                 castep_write(dynamical_system.atoms,
                              filename=write_filename)
+                info("Wrote a configuration {0}.".format(write_filename))
                 os.chdir('..')
                 structure_count += 1
 
     out.close()
     os.chdir(base_dir)
+
+    info("MD Done.")
