434be5a2780f0d97b55de03e217f631f11003127
Tom Daff <7022897+tdaff@users.noreply.github.com>
2015-08-26 14:43:48 +0100

    rewrote slice sample method, write out in line with other methods

--- sats/core/slice_sample.py
+++ sats/core/slice_sample.py
@@ -6,13 +6,12 @@ Generic MCMC 'slice sample' routines.
 
 import os
 import math
-from random import uniform
+from random import uniform, randint
 
 import quippy
 from numpy import dot
 from numpy.linalg import inv
 from quippy import Atoms
-from quippy.dynamicalsystem import DynamicalSystem
 from quippy.io import AtomsWriter
 
 from quippy.potential import Potential, Minim
@@ -22,13 +21,108 @@ from sats.ui.log import info, debug
 from sats.util import Capturing
 
 
+class ParamWriter(object):
+    """
+    Write out configurations to a trajectory and castep files to
+    individual subdirectories.
+
+    """
+
+    def __init__(self, atoms, name, potential, calc_energy=True):
+        """
+        Initialise the writer with the templating atoms and
+        potential.
+        """
+        self.atoms = atoms
+        self.name = name
+        self.potential = potential
+        self.calc_energy = calc_energy
+        self.counter = 0
+
+        # TODO: check naming
+        base_dir = os.getcwd()
+        run_path = '{0}'.format(atoms.info['name'])
+        info("Putting files in {0}.".format(run_path))
+        os.mkdir(run_path)
+        os.chdir(run_path)
+
+        trajectory = 'traj_{0}.xyz'.format(atoms.info['name'])
+        self.out = AtomsWriter(trajectory)
+
+    def write_config(self, params):
+        cell_params = params[:6]
+        # Make a list of 3 item lists
+        atom_params = [self.atoms[0].position] + zip(*[iter(params[6:])] * 3)
+
+        new_atoms = self.atoms.copy()
+        new_atoms.set_cell([[cell_params[0], 0.0, 0.0],
+                            [cell_params[1], cell_params[2], 0.0],
+                            [cell_params[3], cell_params[4], cell_params[5]]])
+
+        new_atoms.set_positions(atom_params)
+        info("Cell volume: {0}".format(new_atoms.get_volume()))
+        if self.calc_energy:
+            self.potential.calc(new_atoms, energy=True, forces=True,
+                                virial=True)
+        # Add to the xyz
+        self.out.write(new_atoms)
+        sp_path = '{0:03d}'.format(self.counter)
+        write_filename = '{0}.{1:03d}'.format(self.atoms.info['name'],
+                                              self.counter)
+        os.mkdir(sp_path)
+        os.chdir(sp_path)
+        castep_write(new_atoms, filename=write_filename, kpoint_spacing=0.015)
+        info("Wrote a configuration {0}.".format(write_filename))
+        os.chdir('..')
+        self.counter += 1
+
+    def write_config_old(self, atoms, params, name, potential):
+        """
+        Write the configuration to a trajectory and a castep input file.
+
+        """
+        cell_params = params[:6]
+        # Make a list of 3 item lists
+        atom_params = [atoms[0].position] + zip(*[iter(params[6:])] * 3)
+
+        new_atoms = atoms.copy()
+        new_atoms.set_cell([[cell_params[0], 0.0, 0.0],
+                            [cell_params[1], cell_params[2], 0.0],
+                            [cell_params[3], cell_params[4], cell_params[5]]])
+
+        new_atoms.set_positions(atom_params)
+        info("Cell volume: {0}".format(new_atoms.get_volume()))
+        potential.calc(new_atoms, energy=True, forces=True, virial=True)
+
+        new_atoms.write(name)
+        info("Written {0}.".format(name))
+
+
+
 def create_density_function(atoms, potential, pressure, temperature, e0=None):
     """
     Create a function that calculates the density under the given conditions
     that requires only a set of parameters. Everything else is contained
     within the closure.
 
+    Parameters
+    ----------
+    atoms : ase.Atoms
+        Template structure used as the basis of the density function
+        calculation.
+    potential : Potential or str
+        A quippy potential or a potential_str to initialise a potential
+        used for calculating the density function.
+    pressure : float
+        Pressure in eV/A^3 of the state point to calcualte the density function.
+    temperature : float
+        Temperature in Kelvin of the state point.
+    e0 : float
+        Energy of the minimised structure. This will be calculated from
+        `atoms` if not given.
+
     """
+
     if e0 is None:
         potential.calc(atoms, energy=True)
         e0 = atoms.energy
@@ -41,9 +135,9 @@ def create_density_function(atoms, potential, pressure, temperature, e0=None):
         atom_params = [atoms[0].position] + zip(*[iter(params[6:])] * 3)
 
         new_atoms = atoms.copy()
-        new_atoms.cell = [[cell_params[0], cell_params[1], cell_params[3]],
-                          [0.0, cell_params[2], cell_params[4]],
-                          [0.0, 0.0, cell_params[5]]]
+        new_atoms.set_cell([[cell_params[0], 0.0, 0.0],
+                            [cell_params[1], cell_params[2], 0.0],
+                            [cell_params[3], cell_params[4], cell_params[5]]])
 
         new_atoms.set_positions(atom_params)
 
@@ -58,154 +152,161 @@ def create_density_function(atoms, potential, pressure, temperature, e0=None):
     return density_function
 
 
-def next_x(density_function, initial_parameters, idx, w_lattice, w_atom, m, f_0=None):
-    # make a copy of the list
-    x_new = initial_parameters[:]
-    x = initial_parameters
+def increment_params(density_function, params, idx, delta, max_steps, df_0=None):
+    """
+    Use slice sampling to generate the next set of parameters changeing the
+    value at index.
 
-    if f_0 is not None:
-        f_slice = uniform(0, 1) * f_0
-    else:
-        f_slice = uniform(0, 1) * density_function(initial_parameters)
+    Parameters
+    ----------
+    density_function : function
+        A function that takes the parameters as an arguments and returns
+        a value of df for those parameters.
+    params : list of float
+        Initial parameters for the density function.
+    idx : integer
+        Index of the parameter to change.
+    delta : float
+        Increment by which to change the value.
+    max_steps : int
+        Maximum number of steps to increment the value during slicing.
+    df_0 : float
+        Value of the density function using the input params
 
-    if idx < 6:
-        x_new_L = initial_parameters[idx] - (w_lattice * uniform(0, 1))
-        x_new_R = x_new_L + w_lattice
-    else:
-        x_new_L = initial_parameters[idx] - (w_atom * uniform(0, 1))
-        x_new_R = x_new_L + w_atom
+    Returns
+    -------
+    (params, df) : (list of float, float)
+        New parameters and the value of the density function at those
+        parameters.
 
-    J = math.floor(m * uniform(0, 1))
-    K = m - 1 - J
+    """
 
-    f_x_new_L = 0.0
-    f_x_new_R = 0.0
-    f_x_new_L_up_to_date = False
-    f_x_new_R_up_to_date = False
+    if df_0 is None:
+        df_0 = density_function(params)
 
-    while J > 0:
-        increase_slice = False
+    params_new = params[:]
 
-        if not f_x_new_L_up_to_date:
-            x[idx] = x_new_L
-            f_x_new_L = density_function(x)
-            f_x_new_L_up_to_date = True
+    # Level of the slice
+    df_slice = uniform(0, 1)*df_0
+    slice_l = params[idx] - uniform(0, 1)*delta
+    slice_r = slice_l + delta
 
-        if f_x_new_L > f_slice:
-            increase_slice = True
+    slice_increments_left = randint(0, max_steps)
 
-        if increase_slice:
-            if idx < 7:
-                x_new_L = x_new_L - w_lattice
-            else:
-                x_new_L = x_new_L - w_atom
-            f_x_new_L_up_to_date = False
-            J -= J
+    # widen the slice to the left until it is no longer above df_slice
+    # or run out of steps
+    for increment in range(slice_increments_left):
+        params_new[idx] = slice_l
+        df_new = density_function(params_new)
+        if df_new > df_slice:
+            slice_l -= delta
         else:
             break
 
-    while K > 0:
-        increase_slice = False
-
-        if not f_x_new_R_up_to_date:
-            x[idx] = x_new_R
-            f_x_new_R = density_function(x)
-            f_x_new_R_up_to_date = True
-
-        if f_x_new_R > f_slice:
-            increase_slice = True
-
-        if increase_slice:
-            if idx < 7:
-                x_new_R = x_new_R + w_lattice
-            else:
-                x_new_R = x_new_R + w_atom
-            f_x_new_R_up_to_date = False
-            K -= 1
+    # use the rest of the steps on the right
+    for increment in range(max_steps-slice_increments_left):
+        params_new[idx] = slice_r
+        df_new = density_function(params_new)
+        if df_new > df_slice:
+            slice_r += delta
         else:
             break
 
+    #
     while True:
-        x_new[idx] = x_new_L + (uniform(0, 1) * (x_new_R - x_new_L))
-
-        f_x_new = density_function(x_new)
-
-        if f_x_new > f_slice:
-            break
-
-        if idx < 7 and x_new_R - x_new_L < w_lattice:
-            break
-        elif x_new_R - x_new_L < w_atom:
-            break
-        elif x_new[idx] < initial_parameters[idx]:
-            x_new_L = x_new[idx]
+        # Get a point in the middle
+        slice_mid = slice_l + uniform(0, 1)*(slice_r-slice_l)
+        params_new[idx] = slice_mid
+        df_new = density_function(params_new)
+        if df_new > df_slice:
+            return params_new, df_new
+        elif slice_r - slice_r < delta/max_steps:
+            # Did it get stuck somewhere and can't get out?
+            return params_new, df_new
+        elif slice_mid < params[idx]:
+            slice_l = params_new[idx]
         else:
-            x_new_R = x_new[idx]
+            slice_r = params_new[idx]
 
-    return x_new, f_x_new
 
 def write_config(atoms, params, name, potential):
+    """
+    Write the configuration to a trajectory and a castep input file.
+
+    """
     cell_params = params[:6]
     # Make a list of 3 item lists
     atom_params = [atoms[0].position] + zip(*[iter(params[6:])] * 3)
 
     new_atoms = atoms.copy()
-    new_atoms.cell = [[cell_params[0], cell_params[1], cell_params[3]],
-                      [0.0, cell_params[2], cell_params[4]],
-                      [0.0, 0.0, cell_params[5]]]
+    new_atoms.set_cell([[cell_params[0], 0.0, 0.0],
+                        [cell_params[1], cell_params[2], 0.0],
+                        [cell_params[3], cell_params[4], cell_params[5]]])
 
-    new_atoms.positions = atom_params
-    print(new_atoms.get_volume())
+    new_atoms.set_positions(atom_params)
+    info("Cell volume: {0}".format(new_atoms.get_volume()))
     potential.calc(new_atoms, energy=True, forces=True, virial=True)
 
     new_atoms.write(name)
+    info("Written {0}.".format(name))
 
 
 def slice_sample(bulk, potential, temperature, pressure, lattice_delta,
                  atom_delta, m_max, e0=None, init_d=0, num_configs=10,
                  write_interval=1):
 
+    info("Inside Slice Sample.")
     if not isinstance(potential, Potential):
         potential = Potential(potential)
 
     bulk = Atoms(bulk)
-    # FIXME pressure in GPa? ->
+    # pressure in GPa -> eV/A^3
     pressure = pressure/quippy.GPA
     density_function = create_density_function(bulk, potential, pressure,
                                                temperature, e0)
 
-    # Convert to triangle lattice representation
-    # TODO: make more transparent
-    latt_inv = inv(bulk.lattice)
+    # Convert to triangle lattice representation (lower triangle in cell)
+    scaled_positions = bulk.get_scaled_positions()
     lattice_params = quippy.get_lattice_params(bulk.lattice)
-    new_lattice = quippy.make_lattice(*lattice_params)
-    bulk.lattice = new_lattice
-    bulk.positions = [dot(new_lattice, dot(latt_inv, pos)) for pos in
-                      bulk.positions]
-
-    new_lattice = new_lattice.tolist()
-    params = [new_lattice[0][0], new_lattice[0][1], new_lattice[1][1],
-              new_lattice[0][2], new_lattice[1][2], new_lattice[2][2]]
+    new_cell = quippy.make_lattice(*lattice_params).T
+    bulk.set_cell(new_cell)
+    bulk.set_scaled_positions(scaled_positions)
+    params = [bulk.cell[0][0], bulk.cell[1][0], bulk.cell[1][1],
+              bulk.cell[2][0], bulk.cell[2][1], bulk.cell[2][2]]
+    info("Re-orineted to cell: {0}.".format(new_cell.tolist()))
 
     for atom in bulk.positions.tolist()[1:]:
-        params.extend(atom.position)
+        params.extend(atom)
 
+    # value for the first iteration
     df_value = density_function(params)
-    print(df_value)
 
-    n = 1
+    # Floating count so that division by write_interval make it integer for
+    # written configurations
+    count = 1.0
     idx = init_d
+    output = ParamWriter(bulk, bulk.info['name'], potential)
 
     # Loop just iterates to the next value of x
-    while n < num_configs*write_interval:
-        params, df_value = next_x(density_function, params, idx, lattice_delta, atom_delta,
-                   m_max, f_0=df_value)
+    while count/write_interval < num_configs:
+        # Determine the delta value outside the incrementer, so we can make it
+        # do less work
+        if idx < 6:
+            delta = lattice_delta
+        else:
+            delta = atom_delta
+        # Here's where the magic happens
+        params, df_value = increment_params(density_function, params, idx,
+                                            delta, m_max, df_0=df_value)
 
-        info("SLICE_SAMPLE: {0}".format(n))
-        if not n%write_interval:
-            write_config(bulk, params, 'slice_sample_{0}.xyz'.format(n), potential)
+        info("SLICE_SAMPLE: {0:g}".format(count/write_interval))
+        info("Params: {0}.".format(", ".join("{0}".format(x) for x in params)))
 
-        n += 1
+        if not count%write_interval:
+            write_config(bulk, params, 'slice_sample_{0:g}.xyz'.format(count/write_interval), potential)
+            output.write_config(params)
+
+        count += 1
         idx += 1
         if idx >= len(params):
             idx = 0
